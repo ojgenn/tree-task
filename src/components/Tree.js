99b797cfdba482070a81d8926b0716159bcf76a9
ojgenn <ojgenn@gmail.comgit>
2018-07-14 13:48:09 +0300

    убрал магическое число в константу

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -7,13 +7,14 @@ export default class Tree extends Component {
       return this.props.data.filter(item => item.parentId === parentId)
     }
     let сurrentId = null;
+    const offsetNumber = 10;
     let bufer = [];
     let renderData = (сurrentId) => {
       let filtredData = getDataFromProps(сurrentId);
       filtredData.forEach(item => {
         let id = item.id;
         let itemLength = item.id.split('_').join('').length - 1;
-        bufer.push(<div key={item.id} style={{marginLeft: itemLength * 10 +'px'}}>{item.title}</div>)
+        bufer.push(<div key={item.id} style={{marginLeft: itemLength * offsetNumber +'px'}}>{item.title}</div>)
         renderData(id);
       });
     }
